56af66539349970d0cb257314a37395dbfd8bcae
Juan Manuel Roldan <juanmanuel.roldan@outlook.com>
2022-09-08 14:11:25 -0300

    flexbox done

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ app.use(express.static(`public`))
 
 /*ROUTES*/
 app.get(`/`, (req,res) => res.sendFile(path.resolve(__dirname, `views`, `home.html`)))
+app.get(`/register`, (req,res) => res.sendFile(path.resolve(__dirname, `views`, `register.html`)))
+app.get(`/login`, (req,res) => res.sendFile(path.resolve(__dirname, `views`, `login.html`)))
+
+
 
 /*SERVER*/
 app.listen(port, () => console.log(`Server running in http://localhost:${port}`))
